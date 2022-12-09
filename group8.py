2f97c615b1fff2b301d569308061fd5a25ede5a5
OmarKazem <114486423+OmarKazem@users.noreply.github.com>
2022-12-09 19:09:10 +0200

    Update group8.py

--- group8.py
+++ group8.py
@@ -45,3 +45,60 @@ goal_arr.insert(0, initial_pos)
 # splitting x and y points into seperate arrays
 x_arr = [item[0] for item in goal_arr]  
 y_arr = [item[1] for item in goal_arr]
+#Creating sensor
+sensor=RangeBearingSensor(robot=veh,map=map,animate=True)
+
+#Printing sensor readings (to detect obstacles)
+print('sensor readings: ', sensor.h(veh.x))
+veh._animation.update(veh.x)
+plt.pause(1)
+ 
+#Defining function for avoiding obstacles
+def detect_obstacles(readings):
+    for i in sensor.h(veh.x):
+        if (i[0] < 6 and abs(i[1]) < pi/4):
+            veh.step(4,pi/3)
+        else: 
+            run = True 
+
+#Defining function for moving to the target while avoiding the obstacles
+def gotarget(goal):
+    goal_heading=atan2(
+            goal[1] - veh.x[1],
+            goal[0] - veh.x[0]
+        )
+    detect_obstacles(sensor.h(veh.x))
+    steer = goal_heading -veh.x[2]
+    if steer>pi:
+        steer =steer-2*pi
+    veh.step(10, steer)
+    veh._animation.update(veh.x)
+    plt.pause(0.005)
+
+#Defining function to make the robot avoid all obstacles and reach all goals as well    
+def gocollision(goal):
+    goal_heading=atan2(
+            goal[1] - veh.x[1],
+            goal[0] - veh.x[0]
+        )
+    detect_obstacles(sensor.h(veh.x))
+    gotarget(goal)
+    steer = goal_heading -veh.x[2]
+    if steer>pi:
+        steer =steer-2*pi
+    veh.step(6, steer)
+    veh._animation.update(veh.x)
+    plt.pause(0.005)
+
+#Loop that uses all three functions to escape the maze and avoids all obstacles
+for n in range(len(goal_arr)-1):
+    run = True
+    goal = [x_arr[n+1], y_arr[n+1]]
+    while (run):
+        gotarget(goal)
+        gocollision(goal)
+        if((abs(goal[0]-veh.x[0]) <0.5) or (abs(goal[1] -veh.x[1]) < 0.5)):
+            break
+        veh._animation.update(veh.x)
+        plt.pause(0.005)
+plt.pause(1000) 
